f6cea3e608b41086fc353637624a24bfa61e8b32
Gardar Mar Jonsson <gardarmar@i-1217c9a8.cs1004cloud.internal>
2016-10-13 21:55:12 +0000

    fleiri test fyrir hvert og refactora negative numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,18 +3,17 @@ package is.ru.stringcalculator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Calculator {
+public class Calculator 
+{
 
 	public static int add(String text){
 		String regex = "[,\\n;]";
-		if(text.equals("")){
+		if(text.equals(""))
+		{
 			return 0;
 		}
-		else if(text.contains("-"))
+		else if(text.contains(",") || text.contains("\n") || text.contains(";") )
 		{
-			throw new IllegalArgumentException("Negatives not allowed: -1");
-		}
-		else if(text.contains(",") || text.contains("\n") || text.contains(";") ){
 			return sum(splitNumbers(text, regex));
 		}
 		else
@@ -32,6 +31,10 @@ public class Calculator {
     private static int sum(String[] numbers){
  	    int total = 0;
         for(String number : numbers){
+			if(toInt(number) < 0)
+			{
+				throw new IllegalArgumentException(NegativeNumber(numbers));
+			}
 			if(toInt(number) <= 1000)
 			{
 				total += toInt(number);
@@ -39,4 +42,33 @@ public class Calculator {
 		}
 		return total;
     }
+	
+	private static String NegativeNumber(String[] numbers) {
+    	String errorMessage = "Negatives not allowed: ";
+    	for(String number : numbers) {
+    		if(toInt(number) < 0) {
+    			errorMessage += number + ", ";
+    		} 
+    	}
+    	if(errorMessage.charAt(errorMessage.length() - 1) == ',') {
+    		errorMessage = errorMessage.substring(0, errorMessage.length() - 2);
+    	}
+    	return errorMessage;
+    }
+	
+	private static int Exceptions(String msg, String regex)
+	{
+		String[] numbers = splitNumbers(msg, regex);
+    	int curr = 0;
+		List<Integer> negNumbers = new ArrayList<Integer>();
+		for(String number : numbers)
+		{
+    		curr = toInt(number);
+		    if(curr < 0)
+		    	negNumbers.add(curr);
+		}
+		if(!negNumbers.isEmpty())
+			throw new IllegalArgumentException("Negatives not allowed: "+ negNumbers.toString());
+		return 1;
+	}
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -23,50 +23,30 @@ public class CalculatorTest
 	@Test
 	public void testTwoNumbers() {
 		assertEquals(3, Calculator.add("1,2"));
+		assertEquals(5, Calculator.add("2,3"));
 	}	
 
 	@Test
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
+		assertEquals(11, Calculator.add("2,2,3,4"));
+    	assertEquals(29, Calculator.add("1,3,3,3,3,3,3,3,7"));
     }
 	@Test
     public void testNumbersWithNewline(){
     	assertEquals(3, Calculator.add("1\n2"));
+		assertEquals(23, Calculator.add("1\n2\n3,4,6,7\n"));
     }
 	@Test
     public void testNumbersWithNewlineAndComma(){
     	assertEquals(6, Calculator.add("1\n2,3"));
     }
-	@Test(expected = IllegalArgumentException.class)
-    public void testNegativeNumbers(){
-    	Calculator.add("1,-1");
+	@Test (expected = IllegalArgumentException.class)
+    public void testNegativeNumbers() {
+    	assertEquals(3, Calculator.add("-1,2"));
+    	assertEquals(2, Calculator.add("1,1"));
+    	assertEquals(2, Calculator.add("2,2,-2,1,-3,-5,2,1"));
     }
-	@Test
-    public void displayNegativeError()
-	{
-    	try
-		{
-			Calculator.add("-1,2");
-		}
-		catch( final IllegalArgumentException ex)
-		{
-			final String error = "Negatives not allowed: -1";
-			assertEquals(error, ex.getMessage());
-		}
-    }
-	@Test
-	public void TestMultipleNegativesError()
-	{
-	  try
-	  {
-		Calculator.add("2,-4,3,-5");
-	  }
-	  catch( final IllegalArgumentException ex )
-	  {
-	    final String msg = "Negatives not allowed: -4, -5";
-	    assertEquals(msg, ex.getMessage());
-	  }
-	}
 
 	@Test
 	public void testNumberSmallerThan1000(){
